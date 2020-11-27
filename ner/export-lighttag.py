8e5cbf681117e1bf58f91e79ef85310256506b39
tenzin <ten13zin@gmail.com>
2020-11-27 12:12:05 +0530

    add citation dataset

--- ner/export-lighttag.py
+++ ner/export-lighttag.py
@@ -6,16 +6,21 @@ import jsonpickle as jp
 jp.set_encoder_options('simplejson', sort_keys=True, indent=4, ensure_ascii=False)
 
 
-def export_to_lighttag(fn):
+def export_to_lighttag(fn, ex_type):
     text = fn.read_text()
     examples = text.split('\n')[:-1]
 
     all_examples = []
     for i, e in enumerate(examples):
-        example = {'order': i, 'ex': e}
+        example = {'order': i, 'ex': e, 'type': ex_type}
         all_examples.append(example)
 
     return all_examples, len(all_examples)
+
+def divide_into_2(l):
+    mid = len(l) // 2
+    return l[:mid], l[mid:]
+
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser(add_help=False)
@@ -23,11 +28,11 @@ if __name__ == "__main__":
     args = ap.parse_args()
 
     path = Path(args.path)
-    lists = path/'lists.txt'
+    #lists = path/'lists.txt'
     citations = path/'citations.txt'
     
-    list_exs, n_lists = export_to_lighttag(lists)
-    citation_exs, n_citations = export_to_lighttag(citations)
+    #list_exs, n_lists = export_to_lighttag(lists, 'list')
+    citation_exs, n_citations = export_to_lighttag(citations, 'citation')
     
     #citations from ebook
     pre_citations = path/'citations.json'
@@ -38,11 +43,9 @@ if __name__ == "__main__":
         citation_exs.extend(citations_json)
         n_citations += len(citations_json)
 
-    output_path = path.parent/'toupload'
-    output_fn = output_path/f'lists-{n_lists}_citations-{n_citations}.json'
 
-    print('[INFO] Exporting to', output_fn)
-    all_examples = list_exs + citation_exs
-    print('[INFO] No. of examples:', len(all_examples))
-    output_fn.write_text(jp.dumps(all_examples))
+    n_citations = 1000
+    citation_exs = citation_exs[n_citations:]
+    output_fn = path.parent/'toupload'/f'citations-{n_citations}:.json'
 
+    output_fn.write_text(jp.dumps(citation_exs))
